83985f3a4aab9c779a59bb208d65d3637fd4b467
Jefferson <jeffarrubla@gmail.com>
2020-01-14 19:39:37 +0000

    Add validations on article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
-	
+	validates :title, presence: true, length: { minimum: 3, maximum: 50 } #  presence: true To ensure there is a tile, length defines the length
+	validates :description, presence: true, length: { minimum: 10, maximum: 300 } # To ensure there is a tile
+
 end
