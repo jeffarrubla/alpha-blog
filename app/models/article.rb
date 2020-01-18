ce772b2906a659dc439e4799cc63d8879b9f44f2
Jefferson <jeffarrubla@gmail.com>
2020-01-18 15:36:57 +0000

    Add association between user and articles and add debuger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
+	belongs_to :user
 	validates :title, presence: true, length: { minimum: 3, maximum: 50 } #  presence: true To ensure there is a tile, length defines the length
 	validates :description, presence: true, length: { minimum: 10, maximum: 300 } # To ensure there is a tile
-
+	validates :user_id, presence: true
 end
