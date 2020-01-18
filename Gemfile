4586bbe12e218186050f5a1d32316ce4bc278242
Jefferson <jeffarrubla@gmail.com>
2020-01-18 18:24:30 +0000

    Add pagination to views

--- Gemfile
+++ Gemfile
@@ -21,6 +21,10 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+# To use pagination
+gem 'will_paginate', '3.1.7'
+gem 'will_paginate-bootstrap4', '0.2.2'
+
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :show, :destroy] #before take any action, for those 4 methods
 
 	def index
-		@articles = Article.all
+		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
 
 	def new
