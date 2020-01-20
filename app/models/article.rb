65a1db358c26ab2952d2de95d6e8b824ca65f8ac
Jefferson <jeffarrubla@gmail.com>
2020-01-20 21:07:32 +0000

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
 	belongs_to :user
+	has_many :article_categories
+	has_many :categories, through: :article_categories
 	validates :title, presence: true, length: { minimum: 3, maximum: 50 } #  presence: true To ensure there is a tile, length defines the length
 	validates :description, presence: true, length: { minimum: 10, maximum: 300 } # To ensure there is a tile
 	validates :user_id, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_18_222327) do
+ActiveRecord::Schema.define(version: 2020_01_20_205808) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
